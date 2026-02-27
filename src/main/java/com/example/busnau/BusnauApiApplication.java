4db41618b6036a470290aa39742bb0ab18a842ab
Mauricio Bedoya <bedoyama@gmail.com>
2026-02-27 14:33:54 -0600

    initial commit

---
+++ src/main/java/com/example/busnau/BusnauApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.busnau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusnauApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusnauApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/busnau/BusnauApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busnau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusnauApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
