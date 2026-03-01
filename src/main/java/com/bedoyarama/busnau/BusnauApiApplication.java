5964a0170a99fd2ce1e3766fb16b04fdb1ec6cec
Mauricio Bedoya <bedoyama@gmail.com>
2026-02-28 18:18:28 -0600

    removed hello world endpoint

--- src/main/java/com/bedoyarama/busnau/BusnauApiApplication.java
+++ src/main/java/com/bedoyarama/busnau/BusnauApiApplication.java
@@ -2,19 +2,11 @@ package com.bedoyarama.busnau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BusnauApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BusnauApiApplication.class, args);
-  }
-
-  @GetMapping("/")
-  public String home() {
-    return "Welcome to the Busnau API!";
   }
 }
